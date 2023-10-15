acc4fb59aa5982b505e190e15333acfd9e03016d
Megan Lake <meganlake95@gmail.com>
2023-10-15 16:14:10 -0700

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,19 +1,7 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/h-thai-ml-tables.png'
-    }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/coffee-cat.jpg'
-    }]
     res.render('places/index', {places})
 })
 
@@ -23,7 +11,19 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
     console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+      // Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
 })
+
 
 module.exports = router
