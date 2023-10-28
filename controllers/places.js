a1281237d5330e35dcb3674d5e950ab14160901e
Megan Lake <meganlake95@gmail.com>
2023-10-28 15:48:51 -0700

    adding rating

--- controllers/places.js
+++ controllers/places.js
@@ -36,7 +36,9 @@ router.post('/', (req, res) => {
 //View Places
 router.get('/:id', (req, res) => {
     db.Place.findById(req.params.id)
+    .populate('comments')
     .then(place => {
+        console.log(place.comments)
         res.render('places/show', { place })
     })
     .catch(err => {
@@ -47,19 +49,6 @@ router.get('/:id', (req, res) => {
 
 
 //Delete Place
-// router.delete('/:id', (req, res) => {
-//     let id = Number(req.params.id)
-//     if (isNaN(id)) {
-//         res.render('error404')
-//     }
-//     else if (!places[id]) {
-//         res.render('error404')
-//     }
-//     else {
-//         places.splice(id, 1)
-//         res.redirect('/places')
-//     }
-// })
 router.delete('/:id', (req, res) => {
     db.Place.findByIdAndDelete(req.params.id)
         .then(deletedplace => {
@@ -70,54 +59,47 @@ router.delete('/:id', (req, res) => {
 })
 
 //View Edit Page
-// router.get('/:id/edit', (req, res) => {
-//     let id = Number(req.params.id)
-//     if (isNaN(id)) {
-//         res.render('error404')
-//     }
-//     else if (!places[id]) {
-//         res.render('error404')
-//     }
-//     else {
-//         res.render('places/edit', { place: places[id], id })
-//     }
-// })
 router.get('/:id/edit', (req, res) => {
-    db.Place.find()
-        .then(foundPlaces => {
-            db.Place.findById(req.params.id)
-                .then(foundPlaces => {
-                    res.render('edit', {
-                        places: foundPlaces
-                    })
-                }).catch(err => {
-                    res.render('error404')
-                })
+    db.Place.findById(req.params.id)
+    .then((place) => {
+    res.render('places/edit', { place })
+    })
+})
+
+//Update/edit a place
+router.put('/:id', (req, res) => {
+    db.Place.findByIdAndUpdate(req.params.id, req.body)
+        .then(() => {
+            res.redirect(`/places/${req.params.id}`)
+        })
+        .catch((err) => {
+            console.log('err', err)
+            res.render('error404')
         })
 })
 
-//Edit Place
-// router.put('/:id', (req, res) => {
-//     let id = Number(req.params.id)
-//     if (isNaN(id)) {
-//         res.render('error404')
-//     }
-//     else if (!places[id]) {
-//         res.render('error404')
-//     }
-//     else {
-//         // Save the new data into places[id]
-//         places[id] = req.body
-//         res.redirect(`/places/${id}`)
-//     }
-// })
-router.put('/:id', (req, res) => {
-    db.Place.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(updatedPlace => {
-        res.redirect(`/breads/${req.params.id}`).catch(err => {
+//Add Comment
+router.post('/:id/comment', (req, res) => {
+    console.log(req.body)
+    db.Place.findById(req.params.id)
+    .then(place => {
+        db.Comment.create(req.body)
+        .then(comment => {
+            place.comments.push(comment.id)
+            place.save()
+            .then(() => {
+                res.redirect(`/places/${req.params.id}`)
+            })
+        })
+        .catch(err => {
             res.render('error404')
         })
     })
+    .catch(err => {
+        res.render('error404')
+    })
 })
+
 
 
 
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,35 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+        <h3 className="inactive">No Comments Yet!</h3>
+    )
+    let rating = (
+        <h3 className="inactive">Not yet rated.</h3>
+    )
+    if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = sumRatings / data.place.comments.length
+        rating = (
+            <h3>
+                {Math.round(averageRating)} stars
+            </h3>
+        )
+        comments = data.place.comments.map(c => {
+            return (
+                <div className="border">
+                    <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                    <stong>- {c.author}</stong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
             <main>
@@ -12,25 +41,63 @@ function show (data) {
                     </div>
                     <div className="col-sm-6">
                         <h1>{data.place.name}</h1>
-                        <h2>Rating</h2>
-                        <p>Currently Unrated</p>
                         <h2>Description</h2>
                         <h4> {data.place.showEstablished()} </h4>
                         <h4> Serving {data.place.cuisines} </h4>
+                        <h2>Rating</h2>
+                        <h4>{rating}</h4>
+                        <br />
+                        <div>
+                            <a href={`/places/${data.place.id}/edit`} className="btn btn-warning"> 
+                                Edit
+                            </a>     
+                            <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}> 
+                                <button type="submit" className="btn btn-danger">
+                                    Delete
+                                </button>
+                            </form>
+                        </div> 
                     </div>
                     <h2>Comments</h2>
-                    <p>No comments yet!</p>
-                    <div>
-                        <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-                            Edit
-                        </a>     
-                        <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-                            <button type="submit" className="btn btn-danger">
-                                Delete
-                            </button>
-                        </form>
-                    </div> 
+                    {comments}
                 </div>
+                <h2>Add Your Own Comment or Rant:</h2>
+                <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+                    <div className="row">
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="author">Author</label>
+                            <input className="form-control" id="author" name="author" />
+                        </div>
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="content">Content</label>
+                            <input className="form-control" id="content" name="content" required />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="form-group col-sm-6">
+                            <select aria-label="Default select example">
+                                <option defaultValue>Star Rating</option>
+                                <option value="0.5">0.5</option>
+                                <option value="1">1</option>
+                                <option value="1.5">1.5</option>
+                                <option value="2">2</option>
+                                <option value="2.5">2.5</option>
+                                <option value="3">3</option>
+                                <option value="3.5">3.5</option>
+                                <option value="4">4</option>
+                                <option value="4.5">4.5</option>
+                                <option value="5">5</option>
+                            </select>
+                        </div>
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="rant">Rant</label>
+                            <br></br>
+                            <input className="form-check-input" type="checkbox" defaultValue="" id="flexCheckChecked" />
+                            <label className="form-check-label" htmlFor="flexCheckChecked"/>
+                        </div>
+                        <input className="btn btn-primary" type="submit" defaultValue="Add Comment" />
+                    </div>
+                </form>
             </main>
         </Def>
     )
